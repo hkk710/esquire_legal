9607039b0250691c1545450ec58517b8c215a5fe
HARIKRISHNA A J <harikrishna@zingmyorder.com>
2023-06-02 01:19:42 +0530

    website setup

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -15,6 +15,182 @@
         </div>
     </div>
 </section>
+<section class="ftco-section">
+        <div class="container-fluid px-md-5">
+        <h2>Mentor</h2>
+            <div class="row">
+                <div class="col-lg-3 col-sm-12">
+                    <div class="block-2 ftco-animate">
+                        <div class="">
+                            <div class="front" style="background-image: url('{{ asset('images/team/mentor.jpg') }}');">
+                                <div class="box">
+                                    <h2>K. G. MOHAN</h2>
+                                    <p>Principal Mentor</p>
+                                </div>
+                            </div>
+                            <div class="back">
+                                <!-- back content -->
+                                <blockquote>
+                                    <p>&ldquo; &rdquo;</p>
+                                </blockquote>
+                                <div class="author d-flex">
+                                    <div class="image align-self-center">
+                                        <img src="{{ asset('images/team/mentor.jpg') }}" alt="">
+                                    </div>
+                                    <div class="name align-self-center ml-3">K. G. MOHAN<span class="position">Principal Mentor</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-sm-12">
+                    <div class=" ftco-animate">
+                        <h2>K. G. MOHAN (FCS, M.Com, CAIIB)</h2>
+                        <p>Principal Mentor</p>
+                        <p>Shri. Mohan is an expert in the legal analysis of complex commercial litigation that is often compounded by significant economic and financial issues. His deep experience has helped minimise the risks associated with litigation by providing unbiased and objective assessments of complex issues, resulting in efficient and effective trial and settlement strategies. In addition, he has been relentlessly guiding counsels to identify documentary and other evidentiary needs along with the development of relevant questions for depositions and interrogatories.</p>
+                        <p>Experience:
+                            <ul>
+                                <li> Over Five decades of expertise in Litigation Advisory, Banking Law, Financial Laws and Corporate Laws including FEMA and SEBI laws. </li>
+                                <li>Post-graduate in Commerce, Member of the Indian Institute of Bankers, Mumbai </li>
+                                <li>Promoter of first S E B I registered Transfer agent from Kerala, Promoter and Managing Director of the first N S E member listed on B S E, from Kerala </li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <h2>ELITE COUNSEL</h2>
+            <div class="row">
+                <div class="col-lg-3 col-sm-12">
+                    <div class="block-2 ftco-animate">
+                        <div class="">
+                            <div class="front" style="background-image: url('{{ asset('images/team/elite1.jpg') }}');">
+                                <div class="box">
+                                    <h2>Adv Dr. KRISS VENUGOPAL</h2>
+                                    <p>B.Tech (Civil) PGDEPM M.Sc (App Psy) LLB</p>
+                                </div>
+                            </div>
+                            <div class="back">
+                                <!-- back content -->
+                                <blockquote>
+                                    <p>&ldquo; &rdquo;</p>
+                                </blockquote>
+                                <div class="author d-flex">
+                                    <div class="image align-self-center">
+                                        <img src="{{ asset('images/team/elite1.jpg') }}" alt="">
+                                    </div>
+                                    <div class="name align-self-center ml-3">Adv Dr. KRISS VENUGOPAL<span class="position">B.Tech (Civil) PGDEPM M.Sc (App Psy) LLB</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-sm-12">
+                    <div class=" ftco-animate">
+                        <h2>Adv Dr. KRISS VENUGOPAL</h2>
+                        <p>B.Tech (Civil) PGDEPM M.Sc (App Psy) LLB</p>
+                        <p>
+                        Kriss as he is lovingly addressed is a Certified Cyber Crime Intervention Officer with an adept background in cyberpsychology and cybercrimes. He embarked on his entrepreneurial stint through his entities like Kriyathmak as its Managing Director and Kaye Inc based out of Dubai as its CEO. He is the Director of La Liberta, involved in training- Soft Skills, corporate training, intercultural communication, voice coaching other than hypnotherapy, and counseling and training in creative writing, Client counselling and administration.</p>
+                        <p>Kriss is an award-winning author. He was recognized twice as one of the Best 100 Indian Authors. He was bestowed with the Rising Star Award from Hubooktique Publishers, the Rabindranath Tagore Literature Award, and Bharat Gaurav Ratan Shri Samman for writing. He is also a Media Personality, Consultant Psychologist, Clinical Hypnotherapist, and Motivational Speaker.</p>
+                        <p>His keen interest in law and jurisprudence made him choose the profession of law. Kriss is proficient in advising Police, Intelligence Services, and Governments and provides legal guidance to management, personnel, or clients.  He provides opinions on complex legal matters and legal strategies to be initiated in litigation, evaluating contracts, regulations, policies, procedures, and standards, and resolves conflicts in policies and procedures. He is adept at client counselling and administration. He represents clients before various Courts, Tribunals, Quasi-Judicial Bodies, and Statutory Authorities. He has expertise in handling Legal Affairs & Litigation, Dispute Resolution, Petitions, Research, and all kinds of legal and advisory services. He has acquired strong legal and professional skills in Negotiation, Legal Compliance, Contracts, joint venture, collaboration, and investment agreements, and governance.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <h2>TEAM OF ADVOCATES</h2>
+            <div class="row">
+                <div class="col-lg-3 col-sm-12">
+                    <div class="block-2 ftco-animate">
+                        <div class="">
+                            <div class="front" style="background-image: url('{{ asset('images/team/adv1.jpg') }}');">
+                                <div class="box">
+                                    <h2>Pankaj Sharma</h2>
+                                    <p>B.A LLB (Hons.)</p>
+                                </div>
+                            </div>
+                            <div class="back">
+                                <!-- back content -->
+                                <blockquote>
+                                    <p>&ldquo; &rdquo;</p>
+                                </blockquote>
+                                <div class="author d-flex">
+                                    <div class="image align-self-center">
+                                        <img src="{{ asset('images/team/adv1.jpg') }}" alt="">
+                                    </div>
+                                    <div class="name align-self-center ml-3">Pankaj Sharma<span class="position">B.A LLB (Hons.)</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-sm-12">
+                    <div class=" ftco-animate">
+                        <h2>Pankaj Sharma</h2>
+                        <p>B.A LLB (Hons.)</p>
+                        <p>Pankaj advises on corporate and Civil Laws, Insolvency and Bankruptcy, and Criminal Laws. He is recognised by clients for his commercial awareness and his solution-driven approach, together with his easy-going manner on a wide range of corporate law matters including private company mergers and acquisitions, joint ventures, company re-organisations, shareholder agreements, share buy-backs, corporate governance, and private equity deals. He regularly represents our clients before the High Courts, lower courts, Tribunals, and Government Agencies</p>
+                        <p>Pankaj assists clients with cases involving:
+                            <ul>
+                                <li>Corporate Law matters</li>
+                                <li>Criminal Defence</li>
+                                <li>Serious drug offences</li>
+                                <li>Sexual offences</li>
+                                <li>Assaults</li>
+                                <li>Financial crime including complex fraud and Proceeds of Crime Act proceedings.</li>
+                                <li>Private prosecutions</li>
+                                <li>Police station representation</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-12">
+                    <div class="block-2 ftco-animate">
+                        <div class="">
+                            <div class="front" style="background-image: url('{{ asset('images/team/adv2.jpg') }}');">
+                                <div class="box">
+                                    <h2>Hemanth Hari</h2>
+                                    <p>M.Com, LLB</p>
+                                </div>
+                            </div>
+                            <div class="back">
+                                <!-- back content -->
+                                <blockquote>
+                                    <p>&ldquo; &rdquo;</p>
+                                </blockquote>
+                                <div class="author d-flex">
+                                    <div class="image align-self-center">
+                                        <img src="{{ asset('images/team/adv2.jpg') }}" alt="">
+                                    </div>
+                                    <div class="name align-self-center ml-3">Hemanth Hari<span class="position">M.Com, LLB</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-sm-12">
+                    <div class=" ftco-animate">
+                        <h2>Hemanth Hari</h2>
+                        <p>M.Com, LLB</p>
+                        <p>Hemanth Hari specialises in lower courts and tribunals. He has excellent legal acumen and handles cases relating to 'Family Laws like Divorce, Maintenance, Guardianship, Arbitration and Conciliation, Civil Laws, Criminal Laws, Consumer Protection, etc. Expertise in handling banking & NBFC-related cases with a pleasing personality and complex client dealing experience. He is specially trained to identify and deal with the sensitivities involved in statutory/regulatory inquiries and civil and criminal proceedings arising out of offences. His promptness and personal investment in our client’s matters have earned us our Client’s trust.</p>
+                        <p>He has several years of experience in dealing with Wills, estate planning, and administration matters. </p>
+                        <p>He also possesses a wealth of experience across the full scope of Family Law matters including Financial Remedy, Child Arrangements, Contested Divorce, Pre- and Post-Nuptial Agreements, Property Disputes, Domestic Abuse, and matters involving children’s services.</p>
+                        <p>He has extensive experience of pursuing and defending domestic commercial arbitrations under various procedural and substantive governing laws.</p>
+                        <p>His areas of practice include the following:
+                            <ul>
+                                <li>Criminal Defence</li>
+                                <li>Lease & Rent Control</li>
+                                <li>Wills & Succession</li>
+                                <li>Matrimonial & Divorce </li>
+                                <li>Cheque Bounce cases</li>
+                                <li>Civil Disputes, Commercial Disputes, Consumer Disputes</li>
+                                <li>Motor Accident Claims, Insurance Claims</li>
+                                <li>Arbitration & Mediation</li>
+                                <li>Drafting, negotiation, and enforcement of contracts and agreements</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @include('partials.footer')
 @include('partials.scripts')
